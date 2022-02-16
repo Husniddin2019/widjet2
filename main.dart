dc3f87678cd972b263d8ad372d4b2167497e28e3
Husniddin2019 <98811010+Husniddin2019@users.noreply.github.com>
2022-02-16 15:31:00 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'Pages/HomePage.dart';
+import 'package:untitled6/pages/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,14 +14,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home:  HomePage(),
-      routes: {
-        HomePage.id:(context) => HomePage(),
-
-      },
+      home: const Homepage(),
+      //const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
